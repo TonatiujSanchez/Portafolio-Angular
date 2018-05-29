85efd45920479ec133f85cf63abed36a589ed04c
Tonatiuj Sanchez <tonsanzjimz@gmail.com>
2018-05-29 01:18:04 -0500

    modificaciones al menu-rutas

---
+++ src/app/components/producto/producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html'
+})
+export class ProductoComponent{
+
+  constructor() { }
+
+
+}
